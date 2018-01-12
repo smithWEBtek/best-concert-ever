12163785987d8cb86717108d9536a6b29929caa4
sabrina-jones <ec2-user@ip-172-31-31-28.us-east-2.compute.internal>
2018-01-12 02:27:54 +0000

    concerts controller

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,2 +1,30 @@
 class ConcertsController < ApplicationController
+   
+   def index
+     @concerts = Concerts.all
+   end
+   
+   def new
+    @concert = Concert.new
+   end
+
+   def create
+     @concert = Concert.new(concert_params)
+
+     if @concert.save
+       redirect_to concert_path(@conert)
+     else
+       render :new
+     end
+   end
+
+    def show
+     @concert = Concert.find(params[:id])
+    end
+
+  private
+    def concert_params
+      params.require(:concert).permit(:name, :artist, :venue)
+    end
+   
 end
