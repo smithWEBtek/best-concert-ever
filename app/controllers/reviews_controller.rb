ff9d823ab304c6a434c5d9820e4dec8af1806653
sabrina-jones <ec2-user@ip-172-31-31-28.us-east-2.compute.internal>
2018-01-12 04:16:22 +0000

    adding view for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,27 @@
 class ReviewsController < ApplicationController
+   def new
+     @review = Review.new
+   end
+   
+   def create 
+       @review = current_user.reviews.build(review_params)
+       
+     if @review.save
+       redirect_to review_path(@review)
+     else
+       render :new
+     end
+   end 
+   
+   def show
+     @review = Review.find(params[:id])
+   end
+   
+   
+   
+  private
+    def review_params
+      params.require(:review).permit(:concert_id, :comment ,:score)
+    end 
+    
 end
