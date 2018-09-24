10897cb9c9cdf6d1226603d506ea92de90f70548
Sabrina Jones <sabrina.jones87@gmail.com>
2018-09-24 02:19:34 +0000

    validation for concerts

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,11 +1,18 @@
 class ConcertsController < ApplicationController
-   
+
    def index
      @concerts = Concert.all
    end
-   
+
    def new
+    binding.pry
     @concert = Concert.new
+    #conditional for if there are errors
+    #store errors in instance variables so that we will have access to them in the new view
+      if flash[:errors]
+        @errors = "name cannot be blank"
+      end
+
    end
 
    def create
@@ -14,7 +21,10 @@ class ConcertsController < ApplicationController
      if @concert.save
        redirect_to concert_path(@concert)
      else
-       render :new
+       # let the user know about errors and redirect to new view
+       #flash[:notice] = "hello"
+       flash[:errors]= @concert.errors.messages
+       redirect_to action: "new"
      end
    end
 
@@ -26,5 +36,5 @@ class ConcertsController < ApplicationController
     def concert_params
       params.require(:concert).permit(:name, :artist, :venue)
     end
-   
+
 end
--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,4 +1,7 @@
+
 class Concert < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+
+    validates :name, presence: true
 end
